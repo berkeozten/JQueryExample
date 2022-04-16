777c7a6fa5025d5027062a058ef1656e43220091
Berke <berkeozten@gmail.com>
2022-04-15 20:06:52 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// $(document).ready(function () {
+
+//     console.log('We are ready!');
+
+// });
+
+// $(document).ready(function () {
+//     $('body').html('<strong>Hello world!</strong>');
+// });
+
+// $(document).ready(function () {
+//     //$('#fire').addClass('highlight');
+//     //$('.solid').addClass('highlight');
+//     //$('.solid, .non-solid').addClass('highlight');
+//     $('#container >>> .non-solid').addClass('highlight');
+
+// });
+
+
+// $(document).ready(function () {
+//     $('#container input:required').addClass('highlight');
+// });
+
+// $(document).ready(function () {
+//     $('#container input[placeholder=E-mail]').addClass('highlight');
+// });
+
+$(document).ready(function () {
+    $('#container input[placeholder*=Name]').addClass('highlight');
+});
